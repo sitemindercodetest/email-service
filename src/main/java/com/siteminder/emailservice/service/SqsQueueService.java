fd8b518a2f079a87d6ef4ef5a64bb65d095cadba
Ashok Ramdoss <ashok.contact@gmail.com>
2019-10-04 20:38:18 +1000

    Extract SQS Queue name to application.properties

--- src/main/java/com/siteminder/emailservice/service/SqsQueueService.java
+++ src/main/java/com/siteminder/emailservice/service/SqsQueueService.java
@@ -3,6 +3,7 @@ package com.siteminder.emailservice.service;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.siteminder.emailservice.model.EmailRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +12,15 @@ public class SqsQueueService implements QueueService {
 
     private final QueueMessagingTemplate queueMessagingTemplate;
 
+    @Value("${sqs.queueName}")
+    private String queueName;
+
     @Autowired
     public SqsQueueService(AmazonSQSAsync amazonSqs) {
         this.queueMessagingTemplate = new QueueMessagingTemplate(amazonSqs);
     }
 
     public void sendMessage(EmailRequest req) {
-        queueMessagingTemplate.convertAndSend("st-email", req);
+        queueMessagingTemplate.convertAndSend(queueName, req);
     }
 }
