417c75ab5fc188e2bb215369341bffa99108f1a2
Ashok Ramdoss <ashok.contact@gmail.com>
2019-10-05 14:14:40 +1000

    Change the SQS Queue to FIFO

--- src/main/java/com/siteminder/emailservice/service/SqsQueueService.java
+++ src/main/java/com/siteminder/emailservice/service/SqsQueueService.java
@@ -7,7 +7,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.cloud.aws.messaging.core.SqsMessageHeaders.SQS_DEDUPLICATION_ID_HEADER;
+import static org.springframework.cloud.aws.messaging.core.SqsMessageHeaders.SQS_GROUP_ID_HEADER;
 
 @Service
 public class SqsQueueService implements QueueService {
@@ -17,6 +22,9 @@ public class SqsQueueService implements QueueService {
     @Value("${sqs.queueName}")
     private String queueName;
 
+    @Value("${sqs.groupId}")
+    private String groupId;
+
     Logger logger = LoggerFactory.getLogger(SqsQueueService.class);
 
     @Autowired
@@ -25,7 +33,12 @@ public class SqsQueueService implements QueueService {
     }
 
     public void sendMessage(EmailRequest req) {
-        queueMessagingTemplate.convertAndSend(queueName, req);
+        Message<EmailRequest> msg = MessageBuilder
+                .withPayload(req)
+                .setHeader(SQS_GROUP_ID_HEADER, groupId)
+                .setHeader(SQS_DEDUPLICATION_ID_HEADER, String.valueOf(req.id))
+                .build();
+        queueMessagingTemplate.send(queueName, msg);
         logger.info("Added message to queue - " + req.id);
     }
 }
